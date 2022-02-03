47e692dc447a24980133cf35aef1410ec308c4a6
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2022-02-03 13:38:03 -0300

    feat: add routes

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+const RoutesApp = () => {
+  return (
+        <BrowserRouter>
+            <Routes>
+              {/*   <Route path="/" element={<Home />} /> */}
+              {/*   <Route path="/teste" element={<Teste />} /> */}
+            </Routes>
+        </BrowserRouter>
+  )
+}
+
+export default RoutesApp
